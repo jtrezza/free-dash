148e8b12b4948b6e927ded7e5216b9a2574b181e
José Trezza <im@jtrezza.com>
2022-12-17 22:52:50 -0500

    Add the parsers and data interfaces for F1 2020

--- src/components/DefaultDashboard.tsx
+++ src/components/DefaultDashboard.tsx
@@ -7,8 +7,10 @@ import { Text, View } from 'react-native';
 import { RemoteInfo, Socket } from 'react-native-udp';
 
 import { packetSize } from 'src/parsers/f120/types';
-import PacketCarTelemetryParser from 'src/parsers/f120/PacketCarTelemetryParser';
-import { PacketCarTelemetryData } from 'src/parsers/f120/types';
+import { PacketMotionParser, PacketSessionParser, PacketLapParser, PacketEventParser,
+  PacketParticipantsParser, PacketCarSetupParser, PacketCarTelemetryParser, PacketCarStatusParser,
+  PacketFinalClassificationParser, PacketLobbyInfoParser } from 'src/parsers/f120/parsers';
+import { PacketCarTelemetryData, PacketParticipantsData, PacketCarSetupData, PacketCarStatusData } from 'src/parsers/f120/types';
 
 type Props = {
   socket: Socket
@@ -16,45 +18,60 @@ type Props = {
 
 const DefaultDashboard = ({ socket }: Props) => {
   const [carTelemetry, setCarTelemetry] = useState({} as PacketCarTelemetryData);
+  const [participants, setParticipants] = useState({} as PacketParticipantsData);
+  const [carSetups, setCarSetups] = useState({} as PacketCarSetupData);
+  const [carStatus, setCarStatus] = useState({} as PacketCarStatusData);
 
   useEffect(()=>{
     const parseFn = (msg: ArrayBuffer, rinfo: RemoteInfo)=>{
 
       switch (rinfo.size) {
         case packetSize.Motion: {
-          //const { data } = new PacketMotionDataParser(msg);
-          console.log('Motion')
+          const buffer = Buffer.from(msg);
+          const parser = new PacketMotionParser();
+          const data = parser.parse(buffer)
+          console.log('Motion data', data)
+          //setCarTelemetry(data)
           break;
         }
-        case packetSize.Session:
-          {
-            //const { data } = new PacketSessionDataParser(msg);
-            console.log('session');
-          }
+        case packetSize.Session: {
+          const buffer = Buffer.from(msg);
+          const parser = new PacketSessionParser();
+          const data = parser.parse(buffer)
+          console.log('Session data', data)
+          //setCarTelemetry(data)
           break;
+        }
         case packetSize.LapData: {
-          //const { data } = new PacketLapDataParser(msg);
-          console.log('lapData');
-
+          const buffer = Buffer.from(msg);
+          const parser = new PacketLapParser();
+          const data = parser.parse(buffer)
+          console.log('Lap data', data)
+          //setCarTelemetry(data)
           break;
         }
         case packetSize.Event: {
-          //const { data } = new PacketEventDataParser(msg);
-          console.log('event');
-
+          const buffer = Buffer.from(msg);
+          const parser = new PacketEventParser();
+          const data = parser.parse(buffer)
+          console.log('Event data', data)
+          //setCarTelemetry(data)
           break;
         }
         case packetSize.Participants: {
-          //const { data } = new PacketParticipantsParser(msg);
-          console.log('participants');
-
+          const buffer = Buffer.from(msg);
+          const parser = new PacketParticipantsParser();
+          const data = parser.parse(buffer)
+          console.log('Participants data', data)
+          setParticipants(data)
           break;
         }
         case packetSize.CarSetups: {
-          //const { data } = new PacketCarSetupDataParser(msg);
-          console.log('carSetups');
-          // log packet size
-
+          const buffer = Buffer.from(msg);
+          const parser = new PacketCarSetupParser();
+          const data = parser.parse(buffer)
+          console.log('Setups data', data)
+          setCarSetups(data)
           break;
         }
         case packetSize.CarTelemetry: {
@@ -64,25 +81,30 @@ const DefaultDashboard = ({ socket }: Props) => {
           setCarTelemetry(data)
           break;
         }
-
         case packetSize.CarStatus: {
-          //const { data } = new PacketCarStatusDataParser(msg);
-          console.log('carStatus');
+          const buffer = Buffer.from(msg);
+          const parser = new PacketCarStatusParser();
+          const data = parser.parse(buffer)
+          console.log('Status data', data)
+          setCarStatus(data)
           break;
         }
-
         case packetSize.FinalClassification: {
-          //const { data } = new PacketFinalClassificationDataParser(msg);
-          console.log('finalClassification');
+          const buffer = Buffer.from(msg);
+          const parser = new PacketFinalClassificationParser();
+          const data = parser.parse(buffer)
+          console.log('Final classification data', data)
+          //setCarStatus(data)
           break;
         }
-
-        case packetSize.LobbyInfo:
-          {
-            //const { data } = new PacketLobbyInfoDataParser(msg);
-            console.log('lobbyInfo');
-          }
-          break;
+        case packetSize.LobbyInfo: {
+          const buffer = Buffer.from(msg);
+          const parser = new PacketLobbyInfoParser();
+          const data = parser.parse(buffer)
+          console.log('Lobby data', data)
+          //setCarStatus(data)
+        }
+        break;
         default:
           break;
       }
@@ -93,6 +115,7 @@ const DefaultDashboard = ({ socket }: Props) => {
 
   return (
     <View style={{justifyContent: 'center', marginLeft: 20}}>
+      <Text style={{fontSize: 60, fontWeight: '700'}}>{participants.m_header ? participants.m_participants[participants.m_header.m_playerCarIndex].m_name : ''}</Text>
       <Text style={{fontSize: 60, fontWeight: '700', paddingLeft: 40}}>{carTelemetry.m_header ? carTelemetry.m_carTelemetryData[carTelemetry.m_header.m_playerCarIndex].m_gear : 0}</Text>
       <Text style={{fontSize: 40, fontWeight: '700'}}>{carTelemetry.m_header ? carTelemetry.m_carTelemetryData[carTelemetry.m_header.m_playerCarIndex].m_speed : 0} km/h</Text>
       <Text style={{fontSize: 40, fontWeight: '700'}}>{carTelemetry.m_header ? carTelemetry.m_carTelemetryData[carTelemetry.m_header.m_playerCarIndex].m_engineRPM : 0} RPM</Text>
---
+++ src/parsers/f120/parsers.ts
@@ -0,0 +1,23 @@
+import PacketMotionParser from './PacketMotionParser';
+import PacketSessionParser from './PacketSessionParser';
+import PacketLapParser from './PacketLapParser';
+import PacketEventParser from './PacketEventParser';
+import PacketParticipantsParser from './PacketParticipantsParser';
+import PacketCarSetupParser from './PacketCarSetupParser';
+import PacketCarTelemetryParser from './PacketCarTelemetryParser';
+import PacketCarStatusParser from './PacketCarStatusParser';
+import PacketFinalClassificationParser from './PacketFinalClassificationParser';
+import PacketLobbyInfoParser from './PacketLobbyInfoParser';
+
+export {
+  PacketMotionParser,
+  PacketSessionParser,
+  PacketLapParser,
+  PacketEventParser,
+  PacketParticipantsParser,
+  PacketCarSetupParser,
+  PacketCarTelemetryParser,
+  PacketCarStatusParser,
+  PacketFinalClassificationParser,
+  PacketLobbyInfoParser
+}
